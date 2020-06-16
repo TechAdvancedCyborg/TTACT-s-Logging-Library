8eec6fbe6ff24fff94317c470539d3b58f629d5f
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-06-16 11:42:26 +0200

    Made code cleaner

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import ttactlogging
+from ttactlogging import Logger
 import time
 
-ttactlogging.Logger.log(time.time(),"Log","Started Test Utility...\n")
-ttactlogging.Logger.log(time.time(),"Warning","This is a test warning log...\n")
-ttactlogging.Logger.log(time.time(),"Error","This is a test error log...\n")
-ttactlogging.Logger.log(time.time(),"Separator","")
-ttactlogging.Logger.log(time.time(),"Info","This is a test information log...\n")
-ttactlogging.Logger.log(time.time(),"Validation","This is a test validation log...\n")
+Logger.log(time.time(),"Log","Started Test Utility...\n")
+Logger.log(time.time(),"Warning","This is a test warning log...\n")
+Logger.log(time.time(),"Error","This is a test error log...\n")
+Logger.log(time.time(),"Separator","")
+Logger.log(time.time(),"Info","This is a test information log...\n")
+Logger.log(time.time(),"Validation","This is a test validation log...\n")
